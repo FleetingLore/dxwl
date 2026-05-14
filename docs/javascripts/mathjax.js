520d030632e83220dc31448aa19ca4f210616721
石山灰 <Macro__9@outlook.com>
2026-05-14 08:35:49 +0800

    转 mathjax

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -1,13 +1,21 @@
-// docs/javascripts/katex.js
-document$.subscribe(({ body }) => {
-  renderMathInElement(body, {
-    delimiters: [
-      { left: "$$", right: "$$", display: true }, // 行间公式
-      { left: "$", right: "$", display: false }, // 行内公式
-      { left: "\\(", right: "\\)", display: false }, // LaTeX 风格行内
-      { left: "\\[", right: "\\]", display: true }, // LaTeX 风格行间
+window.MathJax = {
+  tex: {
+    inlineMath: [
+      ["$", "$"],
+      ["\\(", "\\)"],
     ],
-    throwOnError: false, // 出错时不中断渲染
-    errorColor: "#cc0000", // 错误信息颜色
-  });
+    displayMath: [
+      ["$$", "$$"],
+      ["\\[", "\\]"],
+    ],
+    processEscapes: true,
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex",
+  },
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise();
 });
